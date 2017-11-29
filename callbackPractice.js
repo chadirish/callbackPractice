140f972f461aeb2f938af549df8423510d7e96e4
Chad Irish <chadirish@Chads-MacBook-Pro.local>
2017-11-29 08:45:55 -0700

    completed callbacks

--- callbackPractice.js
+++ callbackPractice.js
@@ -19,7 +19,9 @@ and WHAT YOU SHOULD WRITE is the sayHi function that makes the code above work:
 // 1. Write a function called first that takes in two parameters, an array and a callback function, then invokes the callback function, passing in the first element in the array as it's argument.  (see the sayHi function on line 12 for reference)
 
   // Code Here 
-
+first = (arr, callback) => {
+  callback(arr[0]);
+}
   
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
@@ -33,7 +35,9 @@ first(names, function(firstName){
 // 2. Write a function called last that takes in an array and a callback function, then invokes the callback, passing in the last element in the array as the argument.
 
   //Code Here
-
+  last = (arr, callback) => {
+    callback(arr[arr.length -1]);
+  }
 
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -45,7 +49,9 @@ last(names, function(lastName){
 // 3. Write a function called multiply that takes in three parameters: two numbers and a callback function.  Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 
   //Code Here
-
+multiply = (num1, num2, cb) => {
+  return cb(num1 * num2);
+}
 
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -58,7 +64,15 @@ multiply(4, 3, function(answer){
 // If the name does not exist, invoke the callback with false as the argument.
 
   //Code Here 
-
+contains = (arr, name, cb) => {
+  //for (var i = 0; i < arr.length; i++){
+    if (arr.includes(name)){
+      cb(true);
+    } else {
+      cb(false);
+    }
+  }
+//}
 
 
 
@@ -76,7 +90,16 @@ contains(names, 'Colt', function(result){
 // Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 
   //Code Here
-
+uniq = (arr, cb) => {
+  var noDups = [];
+  for(i = 0; i < arr.length; i++){
+    if(noDups.indexOf(arr[i]) === -1){
+      noDups.push(arr[i]);
+      cb(noDups);
+    }
+  }
+  
+}
 
 
 uniq(names, function(uniqArr){
@@ -87,7 +110,11 @@ uniq(names, function(uniqArr){
 // 6. Write a function called each that takes in an array of names and a callback function. For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 
     //Code Here 
-
+var each = function(arr, cb){
+  arr.forEach(element => {
+    cb("Chad", 3);
+  });
+}
 
 
 each(names, function(item, indice){
@@ -100,6 +127,13 @@ each(names, function(item, indice){
 // When the correct user object is found, invoke the callback with the user object as an argument.
 
 // Code here
+function getUserById (arr, id, cb){
+  for(i = 0; i < arr.length; i++){
+    if(arr[i].id === id){
+      cb(arr[i]);
+    }
+  }
+}
 
 
 
